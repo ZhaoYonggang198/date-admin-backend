d227569b1d1a63a33967f8809c5b7cbf935c579a
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-12-17 20:17:28 +0800

    Question Answer added

---
+++ controllers/questionAnswer.js
@@ -0,0 +1,51 @@
+const logger = require('../utils/logger').logger('controller QuestionAnswer');
+const Question = require('../models/question')
+
+const questionAnswer = async (openid) => {
+  const questions = await Question.questionList()
+  const answers = await Question.getAnswerList(openid)
+  return questions.map(item => {
+    var questionId = item._key
+    var qa = Object.assign(item, {questionId})
+    var answer = answers.filter(ans => {
+      return ans.questionId === questionId
+    })
+    return answer.length > 0 ? Object.assign(qa, answer[0]) : qa
+  })
+}
+
+const updateAnswer = async (ctx) => {
+  try {
+    const result = await Question.putAnswer(ctx.request.body.session_key, 
+      ctx.request.body.questionId,
+      ctx.request.body.answer);
+    ctx.response.type = "application/json";
+    ctx.response.status = 200;
+    ctx.response.body = {result};
+  } catch (err) {
+      ctx.response.status = 404;
+      ctx.response.type = "application/json";
+      ctx.response.body = {error: err.toString()};
+      logger.error('get Question Answer failed: ' + err.message);
+  }
+}
+
+const getQuestionAnswer = async (ctx) => {
+  try {
+    const filename = await questionAnswer(ctx.query.session_key);
+    ctx.response.type = "application/json";
+    ctx.response.status = 200;
+    ctx.response.body = {url : filename};
+  } catch (err) {
+      ctx.response.status = 404;
+      ctx.response.type = "application/json";
+      ctx.response.body = {error: err.toString()};
+      logger.error('get Question Answer failed: ' + err.message);
+  }  
+}
+
+module.exports = {
+  'GET /question-answer': getQuestionAnswer,
+  'POST /answer': updateAnswer,
+  'PUT /answer': updateAnswer
+}
---
+++ models/question.js
@@ -0,0 +1,66 @@
+const logger = require('../utils/logger').logger('question')
+const ArangoDB = require('./arangoDb')
+const config = require('../config')
+const aql = require('arangojs').aql
+
+const db = new ArangoDB(config.arango.userInfo).database
+
+const QuestionCollection = db.collection('Questions')
+
+const AnswerCollection = db.collection('Answers')
+
+const questionList = async => {
+  const query = aql`
+    for doc in ${QuestionCollection}
+      return doc
+  `
+
+  return await db.query(query).then(cursor => cursor.all())
+    .then(doc => {
+      logger.debug('get question list, total num ', doc.length)
+      return doc
+    },
+    err => {
+      logger.error('get question list fail ', err.message)
+    })
+}
+
+const putAnswer = async (openid, questionId, answer) => {
+  const query = aql`
+    UPSERT {openid: ${openid}, questionId: ${questionId}}
+    INSERT {openid: ${openid}, questionId: ${questionId}, answer: ${answer}, dataCreated: DATE_ISO8601(DATE_NOW()), updates: 1}
+    UPDATE {openid: ${openid}, questionId: ${questionId}, answer: ${answer}, updates: OLD.updates + 1, dateUpdate: DATE_ISO8601(DATE_NOW())}
+    IN ${tAnswerCollection}
+    return NEW
+  `
+  return await db.query(query).then(cursor => cursor.next())
+    .then(doc => {
+      logger.debug('update answer success ', JSON.stringify(doc))
+      return doc
+    },
+    err => {
+      logger.error('update answer fail ', err.message)
+    })
+}
+
+const getAnswerList = async (openid) => {
+  const query = aql`
+    for doc in ${AnswerCollection}
+      filter doc.openid == ${openid}
+      return doc
+  `
+  return await db.query(query).then(cursor => cursor.all())
+    .then(doc => {
+      logger.debug('get answer list success')
+      return doc
+    },
+    err => {
+      logger.error('get answer list fail ', err.message)
+    })
+}
+
+module.exports = {
+  questionList,
+  putAnswer,
+  getAnswerList
+}
