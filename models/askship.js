545605aae574b0ff109ca734c42848cabca22d92
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-12-28 20:51:24 +0800

    add source in ask question

--- models/askship.js
+++ models/askship.js
@@ -73,6 +73,7 @@ class AskshipCollection {
           question: doc.question,
           answer: doc.answer,
           status: doc.status,
+          questionId: doc.questionId,
           dateCreated: doc.dateCreated,
           dateUpdate: doc.dateUpdate
         }
