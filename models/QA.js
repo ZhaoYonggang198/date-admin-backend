2f1fbcae67d7017f735f6d8c6db2e7d2d13c626a
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-12-29 14:56:12 +0800

    update QA status

--- models/QA.js
+++ models/QA.js
@@ -30,13 +30,14 @@ const questionClassify = async (openid, answerOpenid, question) => {
   return result
 }
 
-const askQuestion = async (askerOpenId, answerOpenid, question, source) => {
+const askQuestion = async (askerOpenId, answerOpenid, question) => {
   const classify = await questionClassify(askerOpenId, answerOpenid, question.asr)
 
   let questionId = null
 
   if (!classify) {
-    return await Askship.putQuestion(askerOpenId, answerOpenid, questionId, {url: question.url, asr: question.asr, source}, null, 'no-answer')
+    return await Askship.putQuestion(askerOpenId, answerOpenid, questionId, 
+      question, null, 'no-answer')
   }
 
   questionId = `${classify['question-id']}`
