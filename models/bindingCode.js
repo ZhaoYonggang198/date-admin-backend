d590e2df11d9d635327e91bfbc26b9b4f88905e2
Zhao Yonggang <yonggang.zhao@icloud.com>
2019-01-08 20:18:25 +0800

    bind code added

---
+++ models/bindingCode.js
@@ -0,0 +1,29 @@
+const logger = require('../utils/logger').logger('userIds');
+const ArangoDB = require('./arangoDb')
+const config = require('../config');
+const aql = require('arangojs').aql
+
+const db = new ArangoDB(config.arango.userInfo).database
+
+const collection = db.collection("waitingBindingAccount")
+
+const getBindingCode = async (code, source) => {
+  const query = aql`for doc in ${collection} 
+  filter doc.bindingCode == ${code}  and doc.userType =='${source}' and DATE_DIFF(DATE_NOW, doc.timestamp, 'minute') < 5
+  return doc `
+
+  await db.query(query).then(cursor => cursor.next())
+    .then(doc => {
+      logger.debug('get binding code ', doc)
+      return doc
+    },
+    err => {
+      logger.error('saveOpenid fail ', err.message)
+      logger.error(err)
+      throw err
+    })
+}
+
+module.exports = {
+  getBindingCode
+}
