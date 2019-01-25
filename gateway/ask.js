f7f4a95119c436a58c30c6d0c096d7e24c77fb0a
Zhao Yonggang <yonggang.zhao@icloud.com>
2019-01-25 17:00:03 +0800

    gateway get-status added

--- gateway/ask.js
+++ gateway/ask.js
@@ -1,13 +1,19 @@
 const Askship = require('../models/askship')
 const userIds  = require('../models/userIds')
+const logger = require('../utils/logger').logger('gateway-ask')
 
 const postQuestionAnswer = async (userId, param) => {
   const source = param.source
   const questionId = param.questionId ? param.questionId : null
-  const answerId = param.answerId
+  const answerId = param.answerId 
   const openid = await userIds.getOpenid(userId, source)
   const question = { source, asr: param.questionText, media: param.media }
-  const answer = param.answer
+  const answer = param.answer ? param : null
+
+  if (!answerId || !openid) {
+    logger.error('invalid paramter')
+    return {}
+  }
 
   const result = await Askship.putQuestion(openid, answerId, questionId, question, answer)
   await Askship.updateAnswerStatus(result._key, "read")
