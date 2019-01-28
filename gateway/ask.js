a1ccaceaf71ad6b4a3d7e5303573bfccdc9ec9eb
Zhao Yonggang <yonggang.zhao@icloud.com>
2019-01-28 11:29:21 +0800

    add null answer handling

--- gateway/ask.js
+++ gateway/ask.js
@@ -8,7 +8,7 @@ const postQuestionAnswer = async (userId, param) => {
   const answerId = param.answerId 
   const openid = await userIds.getOpenid(userId, source)
   const question = { source, asr: param.questionText, media: param.media }
-  const answer = param.answer ? param : null
+  const answer = param.answer ? param.answer : null
 
   if (!answerId || !openid) {
     logger.error('invalid paramter')
